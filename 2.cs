b50d2398db872a33a43c2bcadccb20845298e6d7
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-10-25 18:33:42 +0300

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadanie2Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Вычисление числа Пи по заданной формуле:");
+            Console.WriteLine("Введите кол-во слагаемых");
+            int Count = int.Parse(Console.ReadLine());
+            double n = 3;
+            double p = 1;
+            for (i=0; i < Count; i++) 
+            {
+                p = p - (1 / n) +( 1 / (n + 2));
+                n = n + 4;
+            }
+            Console.WriteLine($"Вычисленное нами примерное значение числа Пи = {p*4}");
+        }
+    }
+}
---
+++ 3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zadanie3Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программно найдем первое четырехзначное число в ряде чисел Фибоначчи:");
+            double f0 = 0;
+            double f1 = 1;
+            while (f1 < 1000)
+            {
+                double f = f0 + f1;
+                f0 = f1;
+                f1 = f;
+            }
+            Console.WriteLine($"Первым четырехзначным числом в ряде числел Фибоначчи является {f1}");
+            Console.WriteLine("Теперь вычислим кол-во четырехзначных чисел в ряде Фибоначчи:");
+            int count = 0;
+            while (f1 < 10000)
+            {
+                double f = f0 + f1;
+                f0 = f1;
+                f1 = f;
+                count++;
+            }
+            Console.WriteLine($"Кол-во четырехзначных чисел Фибоначчи = {count}");
+        }
+    }
+}
