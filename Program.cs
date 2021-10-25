504790e6b1229247cea3be6e939bcd42fc2d2c8c
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-10-25 19:27:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadanie6Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от одного до ста");
+            int n = int.Parse(Console.ReadLine());
+                while (n < 1 || n > 100)
+                {
+                    Console.WriteLine("Некорректный ввод! Введите число от одного до ста");
+                    n = int.Parse(Console.ReadLine());
+                }
+            if (n%10 == 1 & (n>11 || n <2))
+            {
+                Console.WriteLine($"{n} год");
+            }
+            else if (n % 10 < 5 & n % 10 > 1)
+            {
+                Console.WriteLine($"{n} года");
+            }
+            else
+            {
+                Console.WriteLine($"{n} лет");
+            }
+        }
+    }
+}
